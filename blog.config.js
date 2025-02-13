f0d9ba48d2137a00fc87d79771bee2f670277352
noeFly <noefly@proton.me>
2025-02-13 21:01:57 +0800

    feat: 🚧 測試首頁歡迎語效果

--- blog.config.js
+++ blog.config.js
@@ -65,7 +65,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '《 活力 ▪︎ 團結 ▪︎ 創新 》,Nan-Hu Honor of Guard<br>Testing...',
+    'Nan-Hu Honor of Guard<br>《 活力 》,Nan-Hu Honor of Guard<br>《 活力 ▪︎ 團結 》,Nan-Hu Honor of Guard<br>《 活力 ▪︎ 團結 ▪︎ 創新 》',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
